e547c5a9aead955df8c7236c8cec235d231b08d4
Carlos Daniel <carlosporrasayala@gmail.com>
2024-08-10 10:57:42 -0500

    feat | Crear la funcionalidad para registrar un nuevo cliente

--- AgendaDapper/Controllers/HomeController.cs
+++ AgendaDapper/Controllers/HomeController.cs
@@ -9,9 +9,27 @@ namespace AgendaDapper.Controllers
     {
         private readonly IRepository _repository = repository;
 
+        [HttpGet]
         public IActionResult Index()
         {
             return View(_repository.GetClients());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new Cliente());
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("IdCliente, Nombres, Apellidos, Telefono, Email, Pais, FechaCreacion")] Cliente client)
+        {
+            if (ModelState.IsValid)
+            {
+                _repository.AddClient(client);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(client);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
